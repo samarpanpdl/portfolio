89f9b31e03e13be3a31f691ca086c8fcdb6f713b
samarpanpdl <poudelaashutosh27@gmail.com>
2025-12-21 21:13:35 +0545

    finishings

--- src/app/components/AchievementSection.jsx
+++ src/app/components/AchievementSection.jsx
@@ -1,20 +1,28 @@
+"use client"
 import React from 'react'
+import dynamic from 'next/dynamic'
+const AnimatedNumbers = dynamic(() => {
+    return import('./AnimatedNumbers')}, {ssr:false}); 
 const achievementlist = [
     {
         metric: "Projects",
         value: "2",
+        postfix: "+",
     },
     {
         metric: "Users",
         value: "2",
+        postfix: "+",
     },
     {
         metric: "Awards",
         value: "2",
+        postfix: "+",
     },
     {
         metric: "Years",
-        value: "2+",
+        value: "2",
+        postfix: "+",
     },
     
 
@@ -26,7 +34,14 @@ const AchievementSection = () => {
         {
             achievementlist.map((achievement,index)=>(
                 <div key={index} className='flex flex-col justify-center items-center md:mx-4 sm:mx-0 sm:px-0'>
-                    <h3 className='lg:text-4xl sm:text-sm font-bold text-white sm:mx-0 sm:px-0'>{achievement.value}+</h3> 
+                    <h3 className='lg:text-4xl sm:text-sm font-bold text-white sm:mx-0 sm:px-0'>
+                        {achievement.prefix}
+                        <AnimatedNumbers animateToNumber={parseInt(achievement.value)} includeComma={true} className="text-white text-4xl font-bold"
+                        configs={(_, index)=>{
+                            return {mass: 1, tension: 140 * (index + 1), friction: 100,};
+                        }} />
+                        {achievement.postfix}
+                        </h3> 
                     <p className='text-[#ADB7BE] text-base sm:mx-0 sm:px-0'>{achievement.metric}</p>
                 </div>
             ))
